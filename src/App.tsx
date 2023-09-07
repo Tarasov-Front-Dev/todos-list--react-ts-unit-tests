17c6ad5a005f619ca8bf4d822043f60eeb507330
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2023-09-07 10:25:02 +0300

    All working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import FormNewTodo from "./Components/FormNewTodo/FormNewTodo";
-import Todos from "./Components/Todos/Todos";
+import Todos from "./Components/TodosList/TodosList";
 import FilterTodos, {
   FilterOptions,
 } from "./Components/FilterTodos/FilterTodos";
+import { filteredTodos } from "./Utils/Functions";
 
 export type TodoModel = {
   id: number;
@@ -13,16 +14,19 @@ export type TodoModel = {
 
 function App() {
   const [todos, setTodos] = useState<TodoModel[]>([]);
-  const [filter, setFilter] = useState<keyof typeof FilterOptions>(
-    FilterOptions.ALL
-  );
+  const [filter, setFilter] = useState(FilterOptions.ALL);
 
   return (
     <article className="todos__wrapper">
       <h1>todos</h1>
       <FormNewTodo setTodos={setTodos} />
-      <Todos todos={todos} setTodos={setTodos} />
-      <FilterTodos filter={filter} setFilter={setFilter} setTodos={setTodos} />
+      <Todos todos={filteredTodos(todos, filter)} setTodos={setTodos} />
+      <FilterTodos
+        filter={filter}
+        setFilter={setFilter}
+        setTodos={setTodos}
+        itemsLeft={filteredTodos(todos, filter).length}
+      />
     </article>
   );
 }
